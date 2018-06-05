d6d237c974e370df733c46d1aa77a96330acfb5e
spok7 <spok3161@gmail.com>
2018-06-05 17:54:06 -0400

    untested { range is now uint16_t, implemented update() functionality

--- src/LoopTimer.cpp
+++ src/LoopTimer.cpp
@@ -4,9 +4,10 @@
 LoopTimer::LoopTimer(uint16_t range) {
     Serial.begin(9600);
     delay(3000);
-    Serial.print(F("\nFree Memory: "));
+    Serial.print(F("\nInitial Free Memory: "));
     Serial.println(freeMemory(), DEC);
 
+    firstRun = true;
     this->range = range;
     setupDifference = 0;
     numMinEntries = 1;
@@ -16,7 +17,7 @@ LoopTimer::LoopTimer(uint16_t range) {
     timeDelay = 0;
 
     timestamps = new unsigned long [range];
-    for (int i = 0; i < range; ++i) {
+    for (uint16_t i = 0; i < range; ++i) {
         Serial.print(timestamps[i] = i);
         Serial.print(F(" "));
     }
@@ -27,7 +28,7 @@ LoopTimer::LoopTimer(uint16_t range) {
     Serial.print(F("Range: "));
     Serial.println(range);
 
-    Serial.print(F("Free Memory: "));
+    Serial.print(F("Free Memory after setup: "));
     Serial.println(freeMemory(), DEC);
 }
 
@@ -47,7 +48,7 @@ void LoopTimer::setNumMedianEntries(uint8_t num) {
     numMedianEntries = num;
 }
 
-void LoopTimer::delayByLoops(uint8_t num) {
+void LoopTimer::delayByLoops(uint16_t num) {
     loopDelay = num;
 }
 
@@ -55,20 +56,20 @@ void LoopTimer::delayByTime(uint32_t ms) {
     timeDelay = ms;
 }
 
-static void LoopTimer::sort(unsigned long *list, uint8_t range) {
+static void LoopTimer::sort(unsigned long *list, uint16_t range) {
     unsigned long i, key, j;
     for (i = 1; i < range; ++i) {
         key = list[i];
         j = i - 1;
         while (j >= 0 && list[j] > key) {
             list[j + 1] = list[j];
-            j = j - 1;
+            --j;
         }
         list[j + 1] = key;
     }
 }
 
-static unsigned long LoopTimer::avg(unsigned long *list, uint8_t range) {
+static unsigned long LoopTimer::avg(unsigned long *list, uint16_t range) {
     unsigned long total = 0;
     for (uint8_t i = 0; i < range; ++i) total += list[i];
     return total / range + (total % range == 0 ? 0 : 1);
@@ -82,19 +83,62 @@ void LoopTimer::update() {
         timestamps[loopCount] = millis();
         Serial.print(F("  Timestamp: "));
         Serial.println(timestamps[loopCount]);
+        ++loopCount;
+    } else {
+
+        if (firstRun) {
+            Serial.print("Set up: ");
+            Serial.println(timestamps[0] - setupDifference);
+            firstRun = false;
+        }
+
+        Serial.print("Average: ");
+        Serial.println(avg(timestamps + 1, range - 1));
+
+
+        for (int i = range - 1; i > 0; --i)
+            timestamps[i] -= timestamps[i - 1];
+        sort(timestamps + 1, range - 1);
+
+        // val/count pairs
+        uint16_t currentMean[] = {0, 0};
+        uint16_t potentialMean[] = {0, 0};
+
+        for (uint16_t i = 1; i < range; ++i) {
+            if (timestamps[i] == currentMean[0]) {
+                ++currentMean[1];
+                if (currentMean[1] > potentialMean[1]) {
+                    potentialMean[0] = currentMean[0];
+                    potentialMean[1] = currentMean[1];
+                }
+            } else {
+                currentMean[0] = timestamps[i];
+                currentMean[1] = 1;
+            }
+        }
+
+        Serial.print("Mean: ");
+        Serial.print(potentialMean[0]);
+        Serial.print("\tCount: ");
+        Serial.println(potentialMean[1]);
+
+
+        if (numMinEntries != 0) {
+            Serial.print("Min(s):");
+            for (uint8_t i = 1; i < numMinEntries + 1; ++i) {
+                Serial.print(" ");
+                Serial.print(timestamps[i]);
+            }
+        }
+
+        if (numMaxEntries != 0) {
+            Serial.print("Max(s):");
+            for (uint8_t i = range - 1; i > range - numMaxEntries - 1; --i) {
+                Serial.print(" ");
+                Serial.print(timestamps[i]);
+            }
+        }
+
+        loopCount = 1;
     }
-    // else {
-
-    //     for (int i = range - 1; i > 0; --i) timestamps[i] -= timestamps[i - 1];
-
-    //     // sort(timestamps + 1, range - 1);
-
-    //     // Serial.begin(9600);
-    //     Serial.print("Set up: ");
-    //     Serial.println(timestamps[0]);
-    //     Serial.print("Average: ");
-    //     Serial.println(avg(timestamps + 1, range - 1));
-    //     // Serial.end();
-    // }
-    ++loopCount;
 }
--- src/looptimer.h
+++ src/looptimer.h
@@ -7,20 +7,19 @@
 
 class LoopTimer {
 	private:
-		uint8_t range;
+		bool firstRun;
+		uint16_t range;
+		uint16_t loopCount;
 		unsigned long setupDifference;
 		uint8_t numMinEntries;
 		uint8_t numMaxEntries;
 		uint8_t numMedianEntries;
-		uint8_t loopDelay;
+		uint16_t loopDelay;
 		unsigned long timeDelay;
-
 		unsigned long *timestamps;
-		unsigned long *potato;
-		uint8_t loopCount;
 
-		static void sort(unsigned long *list, uint8_t range);
-		static unsigned long avg(unsigned long *list, uint8_t range);
+		static void sort(unsigned long *list, uint16_t range);
+		static unsigned long avg(unsigned long *list, uint16_t range);
 
 	public:
 		LoopTimer(uint16_t range);
@@ -28,7 +27,7 @@ class LoopTimer {
 		void setNumMinEntries(uint8_t);
 		void setNumMaxEntries(uint8_t);
 		void setNumMedianEntries(uint8_t);
-		void delayByLoops(uint8_t);
+		void delayByLoops(uint16_t);
 		void delayByTime(uint32_t);
 		void update();
 };
